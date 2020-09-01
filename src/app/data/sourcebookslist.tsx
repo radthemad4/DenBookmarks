046d450726d40a83ee96ce4699fcd2dc397e76b6
radthemad4 <radthemad4@yahoo.com>
2020-09-01 20:39:41 +0600

    Update

--- src/app/data/sourcebookslist.tsx
+++ src/app/data/sourcebookslist.tsx
@@ -89,4 +89,21 @@ export const sourcebooks: Entry[] = [
         author: 'Tarkisflux',
         relatedLinks: [{title: 'Den Thread', link: 'https://www.tgdmb.com/viewtopic.php?t=53127'}]
     },
+    {
+        name: "Tome of Prowess",
+        link: 'https://dnd-wiki.org/wiki/Tome_of_Prowess_(3.5e_Sourcebook)',
+        author: 'Tarkisflux',
+        relatedLinks: [{title: 'Den Thread', link: 'https://www.tgdmb.com/viewtopic.php?t=53127'}]
+    },
+    {
+        name: 'AwesomeTome PDF',
+        author: 'Aktariel',
+        link: 'https://github.com/ideologysec/awesometome/releases/tag/rev180',
+        relatedLinks: [{title: 'Den Thread', link: 'http://tgdmb.com/viewtopic.php?p=530589#530589'}],
+    },
+    {
+        name: 'Family Tome',
+        author: 'virgil',
+        link: 'https://docs.google.com/document/d/1d14dFqYOb_saVBWiFPGgeWKg--0EyJ38Mw7CJBQSf0k/edit#heading=h.dkfn4lkh1dik',
+    },
 ];
